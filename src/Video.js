1db3223e18916a5077d876ff483bf5427f13cf91
Krishna Nanda <krishnananda@ucsb.edu>
2021-02-25 21:57:17 -0800

    Fixed Video Links

--- src/Video.js
+++ src/Video.js
@@ -10,23 +10,17 @@ function Video() {
 
     const links = [
         'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
-        'https://www.youtube.com/embed/YoHD9XEInc0',
+        "https://www.youtube.com/embed/IaTcy1Vvbi0",
+        "https://www.youtube.com/embed/2LqzF5WauAw",
+        "https://www.youtube.com/embed/AZGcmvrTX9M",
+        "https://www.youtube.com/embed/vw2FOYjCz38",
+        "https://www.youtube.com/embed/qo5jJpHtI1Y",
+        "https://www.youtube.com/embed/PaZVjZEFkRs",
+        "https://www.youtube.com/embed/-4QPVo0UIzc",
+        "https://www.youtube.com/embed/vgqG3ITMv1Q",
+        "https://www.youtube.com/embed/imm6OR605UI",
+        "https://www.youtube.com/embed/bkYCR677_OQ",
+        "https://www.youtube.com/embed/FSH-dbbiroI"
     ];
 
 
